3c4ecab856dc3051c0b6b4d5e42b2a57a6b42751
Pawan Dhaka <pawandhaka.cse@gmail.com>
2026-06-03 17:02:12 +0530

    Update Paymentpage.js

--- app/components/Paymentpage.js
+++ app/components/Paymentpage.js
@@ -66,7 +66,7 @@ const Paymentpage = ({ username }) => {
             "description": "Test Transaction",
             "image": "https://i.pinimg.com/originals/8e/cb/5b/8ecb5bda69e29eb348a04ad66077fac6.gif",
             "order_id": orderId, // This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-            "callback_url": "http://localhost:3000/api/razorpay",
+            "callback_url": "https://get-me-a-mango.vercel.app//api/razorpay",
             "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
                 "name": "Gaurav Kumar", //your customer's name
                 "email": "gaurav.kumar@example.com",
